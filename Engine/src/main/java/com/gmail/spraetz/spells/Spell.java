2a03edc46793e46ea9504f701f34123b8d4c1bb7
Ryan Spraetz <spraetz@gmail.com>
2014-02-17 16:51:03 -0800

    Spellbooks lose charges when used.

--- Engine/src/main/java/com/gmail/spraetz/spells/Spell.java
+++ Engine/src/main/java/com/gmail/spraetz/spells/Spell.java
@@ -6,10 +6,14 @@ import org.bukkit.entity.Fireball;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.metadata.FixedMetadataValue;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by spraetz on 2/16/14.
@@ -23,6 +27,7 @@ public abstract class Spell {
     public Spell(PlayerEvent event, Engine plugin){
         this.player = event.getPlayer();
         this.plugin = plugin;
+        this.event = event;
     }
 
     public static String getName() throws Exception {
@@ -36,9 +41,22 @@ public abstract class Spell {
     }
 
     public boolean cast(){
+
+        PlayerInteractEvent e = (PlayerInteractEvent)event;
+
+        System.out.println(e.getPlayer().getName());
+        System.out.println(e.getPlayer().getItemInHand().getType());
+
         // Check if there are charges on the spellbook
+        Integer charges = getCharges(event.getPlayer().getItemInHand());
 
         // Remove one charge
+        if(charges == 0){
+            player.sendMessage("Your spellbook is out of charges!");
+            return false;
+        }
+
+        setCharges(event.getPlayer().getItemInHand(), charges-1);
 
         // Cause spell effects
         spellEffects(event);
@@ -62,5 +80,22 @@ public abstract class Spell {
         spellMap.put(Teleport.getName(), Teleport.class);
 
         return spellMap;
+    }
+
+    public static String chargeString(Integer numberOfCharges){
+        return "charges: " + numberOfCharges;
+    }
+
+    public static Integer getCharges(ItemStack spellbook){
+        ArrayList<String> loreList = (ArrayList<String>)spellbook.getItemMeta().getLore();
+        return Integer.parseInt(loreList.get(0).split(" ")[1]);
+    }
+
+    public static void setCharges(ItemStack spellbook, Integer numberOfCharges){
+        ItemMeta itemMeta = spellbook.getItemMeta();
+        ArrayList<String> loreList = (ArrayList<String>)itemMeta.getLore();
+        loreList.set(0, chargeString(numberOfCharges));
+        itemMeta.setLore(loreList);
+        spellbook.setItemMeta(itemMeta);
     }
 }
